b6fead2c90241aa04296829db72c3e8f207af22b
Alessandro Di Maria <adm@m42.ch>
2020-11-14 10:08:05 +0100

    init app, with product and description

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-const product = 'Socks'
+const app = Vue.createApp({
+	// data: function () {
+	data() {
+		return {
+			product: "Socks",
+			description: "very comfortable socks that give warm feet"
+		}
+	}
+});
