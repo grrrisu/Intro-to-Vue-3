88fabf156e4c65faadad0b50f0f2c35f129398b0
Alessandro Di Maria <adm@m42.ch>
2020-11-14 10:28:51 +0100

    add image and url

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ const app = Vue.createApp({
 	data() {
 		return {
 			product: "Socks",
-			description: "very comfortable socks that give warm feet"
+			description: "very comfortable socks that give warm feet",
+			image: "./assets/images/socks_green.jpg",
+			url: "http://www.socks.com"
 		}
 	}
 });
