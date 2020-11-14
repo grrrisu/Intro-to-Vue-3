4bca2e06fd62c3129cbc3a5c99dc99d25700dcbc
Alessandro Di Maria <adm@m42.ch>
2020-11-14 10:40:53 +0100

    out of stock, on sale

--- main.js
+++ main.js
@@ -5,7 +5,9 @@ const app = Vue.createApp({
 			product: "Socks",
 			description: "very comfortable socks that give warm feet",
 			image: "./assets/images/socks_green.jpg",
-			url: "http://www.socks.com"
+			url: "http://www.socks.com",
+			stock: 10,
+			sale: true
 		}
 	}
 });
